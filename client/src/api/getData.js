d9f5d0b2e4fd6d49b6607c7b255e0c239b858d5b
Edu Quiroz <paincore.px@gmail.com>
2023-02-22 16:01:25 -0600

    corrigiendo ruta API2

--- client/src/api/getData.js
+++ client/src/api/getData.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-export const getStationsRequests = async () => await axios.get('/api/v1/stations')
-export const getStationsByLineRequests = async (id) => await axios.get('/api/v1/lines/' + id)
-export const getLinesRequests = async () => await axios.get('/api/v1/lines')
+export const getStationsRequests = async () => await axios.get('https://stcapp-production.up.railway.app/api/v1/stations')
+export const getStationsByLineRequests = async (id) => await axios.get('https://stcapp-production.up.railway.app/api/v1/lines/' + id)
+export const getLinesRequests = async () => await axios.get('https://stcapp-production.up.railway.app/api/v1/lines')
